3e85f31a747a72e95717dacdd055c4b4eb366ebf
William <wllfl@ig.com.br>
2014-11-17 19:57:40 -0200

    Correção JSON

--- exemplo3/consulta_json.php
+++ exemplo3/consulta_json.php
@@ -3,7 +3,7 @@ $acao = (isset($_POST['acao'])) ? $_POST['acao'] : '' ;
 $pdo = new PDO("pgsql:host=localhost;dbname=blog;", 'blog', '123456');
 
 if($acao == 'carregar_json'):
-	$sql = 'SELECT descricao FROM tab_uf';
+	$sql = 'SELECT descricao FROM tab_cidade';
 	$stm = $pdo->prepare($sql);
 	$stm->execute();
 	$dados = $stm->FetchAll(PDO::FETCH_OBJ);
